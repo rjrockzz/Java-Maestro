ff8de1881d297fdf549935c230ba1862536cea61
Arjun Sharma <500060811@stu.upes.ac.in>
2019-07-01 11:40:49 +0530

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        for(int x : arr)
+        {
+            System.out.println(x);
+        }
+    }
+}
