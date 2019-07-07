41457e6e3baa95e6afe2de9d72c1a270d76888da
codιng вloĸe <500060811@stu.upes.ac.in>
2019-07-08 00:53:01 +0530

    Add files via upload

---
+++ CharSequnceInterface.java
@@ -0,0 +1,7 @@
+public class CharSequnceInterface {     //An interface which is implemented by the String Class 
+    public static void main(String[] args) {
+        CharSequence ch = "ABC";
+        System.out.println(ch.charAt(1));
+        System.out.println(ch.length());
+    }
+}
