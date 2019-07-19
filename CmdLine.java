27abb6ea4ffb0651d43ee27ba4d61f7308804415
codιng вloĸe <500060811@stu.upes.ac.in>
2019-07-19 17:36:05 +0530

    Add files via upload

---
+++ CmdLine.java
@@ -0,0 +1,9 @@
+public class CmdLine {
+    public static void main(String[] args) {
+        for(String x : args)
+        {
+            System.out.println(x+" ");
+            System.out.println();
+        }
+    }
+}
