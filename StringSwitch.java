8a2cf4fb0e21f47fd80104072fdbf805277e9338
codιng вloĸe <500060811@stu.upes.ac.in>
2019-07-22 17:39:01 +0530

    Add files via upload

---
+++ StringSwitch.java
@@ -0,0 +1,9 @@
+public class StringSwitch {
+    public static void main(String[] args) {
+        if (true)
+            if (false)
+                System.out.println("a");
+            else
+                System.out.println("B");
+    }
+}
