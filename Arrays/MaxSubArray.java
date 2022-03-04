37f229a4bd01007d2fe34f133dddebce67b941e7
Arjun <arjun.sharma@skuad.io>
2022-03-04 09:31:38 +0530

    MaxSubArray

---
+++ Arrays/MaxSubArray.java
@@ -0,0 +1,15 @@
+public class MaxSubArray {
+    public static int MaxSub(int[] nums){
+        if(nums.length == 0) return 0;
+        int curMax = nums[0], curSum = 0;
+        for(int num : nums){
+            curSum = Math.max(num,num+curSum);
+            curMax = Math.max(curMax, curSum);
+        }
+        return curMax;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MaxSub(new int[]{-1,-2}));
+    }
+}
