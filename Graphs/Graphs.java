1d5ab82b8a5b6f1208280685ded51e757f0bea62
Arjun <arjun.sharma@skuad.io>
2022-03-07 22:44:49 +0530

    Graphs

---
+++ Graphs/Graphs.java
@@ -0,0 +1,46 @@
+/*
+    Vertices : Nodes
+    Ordered pair of form (u,v) called as edge (for directed graph).
+
+    2 common reps of a graph :
+    - Adjacency Matrix
+    - Adjacency List
+ */
+
+import java.util.*;
+
+public class Graphs {
+
+    static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v){
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+    static void
+    printGraph(ArrayList<ArrayList<Integer>> adj){
+        for (int i = 0; i < adj.size(); i++){
+            System.out.println("Adjacency List of Vertex : " + i );
+            System.out.println("head");
+            for(int j = 0; j < adj.get(i).size(); j++){
+                System.out.println(" -> " + adj.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
+        for(int i = 0; i < V; i++)
+            adj.add(new ArrayList<Integer>());
+
+        addEdge(adj,0,1);
+        addEdge(adj,0,4);
+        addEdge(adj,1,2);
+        addEdge(adj,1,3);
+        addEdge(adj,1,4);
+        addEdge(adj,2,3);
+        addEdge(adj,3,4);
+
+        printGraph(adj);
+    }
+}
